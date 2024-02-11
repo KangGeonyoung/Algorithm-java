373cca21a44abf04f98e0f12ae0adb14502585c4
KangGeonyoung <kky6335@gmail.com>
2024-02-11 22:02:12 +0900

    백준: 단계별로 풀어보기 - 입출력과 사칙연산: 11382번 문제 - 조건문: 2884번 문제

---
+++ src/main/java/org/baekjoon/practice/Problem_11382.java
@@ -0,0 +1,15 @@
+package org.baekjoon.practice;
+
+import java.util.Scanner;
+
+public class Problem_11382 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        long a = kb.nextLong();
+        long b = kb.nextLong();
+        long c = kb.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
