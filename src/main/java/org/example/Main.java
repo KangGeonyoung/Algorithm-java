095e664ec82b335bc13354074c63d2e5b3227288
KangGeonyoung <kky6335@gmail.com>
2024-02-12 22:36:52 +0900

    백준: 단계별로 풀어보기 - 문자열: 2743번, 9086번, 11718번, 27866번 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        int[] numbers = new int[30];
 
-        for (int i = 0; i < 28; i++) {
-            numbers[kb.nextInt() - 1] = 1;
-        }
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] != 1) {
-                System.out.println(i + 1);
-            }
+        while(kb.hasNext()) {
+            System.out.println(kb.nextLine());
         }
     }
 }
