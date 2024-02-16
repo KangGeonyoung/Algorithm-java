4f607124bd719ca54db556d9455db0503e3cbc80
KangGeonyoung <kky6335@gmail.com>
2024-02-17 00:53:44 +0900

    백준: 단계별로 풀어보기 - 약수, 배수와 소수: 2501번, 9506번 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        int point = 2;
 
-        int N = kb.nextInt();
-        for (int i = 0; i < N; i++) {
-            point += Math.pow(2, i);
-        }
-        int totalPoint = point * point;
-
-        System.out.println(totalPoint);
     }
 }
