0101f26d37b7b1f9f5d82746a915643562509068
KangGeonyoung <kky6335@gmail.com>
2024-03-09 00:36:23 +0900

    백준

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,24 +9,6 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        int[] weight = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            weight[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(weight);
-
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            if (sum + 1 < weight[i]) {
-                break;
-            }
-            sum += weight[i];
-        }
-        bw.write(Integer.toString(sum + 1));
         bw.close();
     }
 }
