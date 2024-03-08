564e5024cbf111a4141eab1f6f3611b3cff82f2a
KangGeonyoung <kky6335@gmail.com>
2024-03-09 00:35:53 +0900

    백준: 그리디 알고리즘 - 2437번 문제

--- src/main/java/org/baekjoon/greedy/Problem_2437.java
+++ src/main/java/org/baekjoon/greedy/Problem_2437.java
@@ -1,9 +1,10 @@
-package org.example;
+package org.baekjoon.greedy;
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Problem_2437 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
